a85eec77f60261214d8cf454bac847718799c84e
Monica Delgado <monicadelgadod11@gmail.com>
2025-02-25 19:10:51 +0100

    v-for, v-on, v-if

--- app.js
+++ app.js
@@ -1,27 +1,31 @@
-const app =  createApp({
+const {createApp, ref} = Vue;
+
+const sententiae = [
+    { sententia: 'The night is darkest just before the dawn. And I promise you, the dawn is coming.', auctor: 'Harvey Dent, The Dark Knight' },
+    { sententia: 'I believe what doesn’t kill you simply makes you, stranger.', auctor: 'The Joker, The Dark Knight' },
+    { sententia: 'Your anger gives you great power. But if you let it, it will destroy you… As it almost did me', auctor: 'Henri Ducard, Batman Begins' },
+    { sententia: 'You either die a hero or live long enough to see yourself become the villain.', auctor: 'Harvey Dent, The Dark Knight' },
+    { sententia: 'If you’re good at something, never do it for free.', auctor: 'The Joker, The Dark Knight' },
+    { sententia: 'Yes, father. I shall become a bat.', auctor: 'Bruce Wayne/Batman, Batman: Year One' },
+]
 
 
-    template: `
-    <h1>{{ nuntius }}</h1>
-    <h2>{{ auctor }}</h2>
-    `,
-
-    setup() {
-        const nuntius = ref("I'm Batman");
-        const auctor = ref("Batman");
-
-        setTimeout(() =>{
-            nuntius.value = 'Soy Goku';
-            auctor.value = 'Goku';
-            console.log(nuntius);
-        }, 1000);
-        
-        return{
-            nuntius,
-            auctor,
-        }
+const app = createApp({
     
-    }   
+    setup() {
+
+        const revelareAuctor = ref(true);
+
+        const vectisAuctor = () => {
+            revelareAuctor.value = !revelareAuctor.value;
+        }
+
+        return {
+            sententiae, 
+            revelareAuctor,
+            vectisAuctor
+        }
+    }
 });
 
-app.mount('#myApp')
+app.mount('#myApp');
---
+++ intro/app.js
@@ -0,0 +1,35 @@
+const {createApp,ref} = Vue;
+
+const app =  createApp({
+
+
+    //template: `
+    //<h1>{{ nuntius }}</h1>
+    //<h2>{{ auctor }}</h2>
+    //`,
+
+    setup() {
+        const nuntius = ref("I'm Batman");
+        const auctor = ref("Batman");
+
+        //setTimeout(() =>{
+            //nuntius.value = 'Soy Goku';
+            //auctor.value = 'Goku';
+            //console.log(nuntius);
+       // }, 1000);
+        
+        const mutareSentencia = () => {
+            nuntius.value = 'Hola, soy Goku';
+            auctor.value = 'Goku';
+        }
+
+        return{
+            nuntius,
+            auctor,
+            mutareSentencia,
+        }
+    
+    }   
+});
+
+app.mount('#myApp')
